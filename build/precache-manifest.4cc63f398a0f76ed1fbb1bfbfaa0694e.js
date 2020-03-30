9ce0f994af055d2d0a41495a12e2ff2c1995470a
Cezara Luca <cezaraluca@yahoo.com>
2020-03-30 18:04:20 +0300

    first build commit

---
+++ build/precache-manifest.4cc63f398a0f76ed1fbb1bfbfaa0694e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4fa3cd5a0ed35edb1a2dde5143f9ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7631ced1e5e6266e071",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "2e40f7b583362793cf48",
+    "url": "/static/js/2.fc9dd90b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fc9dd90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7631ced1e5e6266e071",
+    "url": "/static/js/main.f44f85c5.chunk.js"
+  },
+  {
+    "revision": "e41272e8719789a85ed7",
+    "url": "/static/js/runtime-main.557f10fb.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+import uuid from "uuid";
+import axios from "axios";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    todos: []
+    /*      {
+        id: uuid.v4(),
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: "Dinner with husband",
+        completed: true
+      },
+      {
+        id: uuid.v4(),
+        title: "Meeting with boss",
+        completed: false
+      }
+    ] */
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => this.setState({ todos: res.data }));
+  }
+
+  //Toggle complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  //DeleteToDo
+  delTodo = id => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    );
+    /* this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    }); */
+  };
+
+  //Add Todo
+  addTodo = title => {
+    /*  const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }; */
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title,
+        completes: false
+      })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+    //this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+/*function App() {
+  return (
+    <div className="App">
+       <h1> App </h1>
+    </div>
+  );
+} */
+
+export default App;
